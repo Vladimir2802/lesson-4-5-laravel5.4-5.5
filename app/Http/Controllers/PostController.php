cec9c2b0d7e879f0b008db45fff41aa73c40f82e
SergeyK <skinsidex@gmail.com>
2017-10-05 13:04:50 +0300

    Lesson 4 - Laravel

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\PostRequest;
+use App\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Post $post)
+    {
+        $posts = $post->orderBy('id', 'asc')->get();
+
+//        dd($posts);
+
+//        return view('post.index', ['posts' => $posts]);
+        return view('post.index', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('post.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Post $post, PostRequest $request)
+    {
+        $post->create($request->all());
+
+        return redirect()->route('post.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * TODO: $id -> $post
+     *
+     * @param  Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        return view('post.show', compact('post'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        return view('post.edit', compact('post'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  PostRequest  $request
+     * @param  Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(PostRequest $request, Post $post)
+    {
+        $post->update($request->all());
+        return redirect()->route('post.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        $post->delete();
+
+        return redirect()->route('post.index');
+    }
+}
---
+++ resources/views/post/_form.blade.php
@@ -0,0 +1,5 @@
+<div class="form-group">
+    {!!Form::label('message', 'Message') !!}
+    {!!Form::text('message', null, ['class' => 'form-control']) !!}
+</div>
+
